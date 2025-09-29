679dc1f1802e1c7f9a732ae58bc992f9d40e9ec3
Aleksey Yesin <esinru@gmail.com>
2025-09-29 16:42:28 +0300

    Add users, companies resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
 import { ServicesModule } from './services/services.module';
+import { User } from './users/entities/user.entity';
+import { Company } from './companies/entities/company.entity';
 import { Service } from './services/entities/service.entity';
 
 @Module({
@@ -18,11 +22,13 @@ import { Service } from './services/entities/service.entity';
         username: configService.get('DB_USERNAME'),
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_DATABASE'),
-        entities: [Service],
+        entities: [User, Company, Service],
         synchronize: false,
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
+    CompaniesModule,
     ServicesModule,
   ],
 })
