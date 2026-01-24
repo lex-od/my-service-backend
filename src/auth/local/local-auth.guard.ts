b83b0d6244898cb83de7b419f7f2900bfe706a0f
Aleksey Yesin <esinru@gmail.com>
2026-01-24 20:24:27 +0200

    Add credentials pre-validation on login

--- src/auth/local/local-auth.guard.ts
+++ src/auth/local/local-auth.guard.ts
@@ -1,5 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  ExecutionContext,
+  BadRequestException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { plainToInstance } from 'class-transformer';
+import { validate, type ValidationError } from 'class-validator';
+import { type Request } from 'express';
+import { LoginDto } from '../dto/login.dto';
+
+function formatValidationErrors(errors: ValidationError[]) {
+  return errors.map((error) =>
+    Object.values(error.constraints || {}).join(', '),
+  );
+}
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const dto = plainToInstance(LoginDto, request.body);
+    const errors = await validate(dto);
+
+    if (errors.length) {
+      const messages = formatValidationErrors(errors);
+      throw new BadRequestException(messages);
+    }
+    return super.canActivate(context) as Promise<boolean>;
+  }
+}
